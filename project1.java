45d8957ff9fab3ef7b951672d42860358cf83626
bharathhalemane <141356210+bharathhalemane@users.noreply.github.com>
2024-09-01 00:52:35 +0530

    Hello Word
    Write a program that takes a word W as input and prints "Hello" followed by the given word W.

---
+++ project1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class project1{
+    public static void main(String[] args) { 
+        // Write your code here
+        Scanner W=new Scanner(System.in);
+        String word=W.nextLine();
+        W.close();
+        System.out.printf("Hello %s",word);
+        
+    }
+}
