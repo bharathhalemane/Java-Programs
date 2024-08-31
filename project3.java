9a52f513466302931fbe35e6d0b06d93c633fd90
bharathhalemane <141356210+bharathhalemane@users.noreply.github.com>
2024-09-01 00:57:54 +0530

    Second line
    this program read two lines of input and print the second line of input.

---
+++ project3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class project3{
+    public static void main(String[] args) { 
+            Scanner sc=new Scanner(System.in);
+            String fun=sc.nextLine();
+            String py=sc.nextLine();
+            System.out.println(py);
+    }
+}
