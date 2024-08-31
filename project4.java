071ebb9ba627c5f45393b1684603a1d24fe2e6c9
bharathhalemane <141356210+bharathhalemane@users.noreply.github.com>
2024-09-01 01:00:00 +0530

    Print line in reverse order
    This program reads two lines of input and prints those two lines in reverse order.

---
+++ project4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class project4 {
+    public static void main(String[] args) { 
+        // Write your code here
+        Scanner sc=new Scanner(System.in);
+        String book=sc.nextLine();
+        String pen=sc.nextLine();
+        System.out.println(pen);
+        System.out.println(book);
+    }
+}
