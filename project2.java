523e9a63a4a8a5a1bc723997ef7b8c94de3ca93d
bharathhalemane <141356210+bharathhalemane@users.noreply.github.com>
2024-09-01 00:54:39 +0530

    Print the Input
    program that reads a single line of input and print the given input.

---
+++ project2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class project2{
+    public static void main(String[] args) { 
+        // Write your code here
+        Scanner W=new Scanner(System.in);
+        String str=W.next();
+        //String Int=W.next();
+        System.out.println(str);
+        //System.out.println(Int);
+    }
+}
