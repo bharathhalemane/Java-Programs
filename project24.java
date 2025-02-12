5bf844fcc40e65c86a96297de093dd01187bdf7a
Bharath Halemane H V <141356210+bharathhalemane@users.noreply.github.com>
2025-02-12 21:11:07 +0530

    eligible criteria-2

---
+++ project24.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class project24{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the marks of Maths: ");
+        int m=sc.nextInt();
+        System.out.print("Enter the marks of Physics: ");
+        int p=sc.nextInt();
+        System.out.print("Enter the marks of Chemistry: ");
+        int c=sc.nextInt();
+//condition: Sum of marks in any two subjects >=100 and m+p+c >=180
+        if (m+p>=100 && m+c>=100 && m+p+c>=180){
+            System.out.println("True");
+        } else if (m+p>=100 && p+c>=100 && m+p+c>=180) {
+            System.out.println("True");
+        } else if (m+c>=100 && p+c>=100 && m+p+c>=180) {
+            System.out.println("True");
+        } else{
+            System.out.println("False");
+        }
+    }
+}
