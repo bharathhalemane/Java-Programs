7cec85369c03b97cc6b8ac87fcb2054f7a53dd70
Bharath Halemane H V <141356210+bharathhalemane@users.noreply.github.com>
2025-02-15 13:09:29 +0530

    print input

--- Problem-02.java
+++ Problem-02.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class project2{
+class Problem02{
     public static void main(String[] args) { 
         // Write your code here
         Scanner W=new Scanner(System.in);
