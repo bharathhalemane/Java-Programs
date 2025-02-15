3d7ed94c0ada7807f0ad38609954381266a78edb
Bharath Halemane H V <141356210+bharathhalemane@users.noreply.github.com>
2025-02-15 13:08:46 +0530

    Hello World

--- Problem-01.java
+++ Problem-01.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class project1{
+class Problem01{
     public static void main(String[] args) { 
         // Write your code here
         Scanner W=new Scanner(System.in);
