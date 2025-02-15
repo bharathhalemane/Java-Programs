61ca0a85e79e78bade26fea2cb46438af19b6a70
Bharath Halemane H V <141356210+bharathhalemane@users.noreply.github.com>
2025-02-15 13:10:30 +0530

    print input

--- Problem-02.java
+++ Problem-02.java
@@ -3,6 +3,7 @@ class Problem02{
     public static void main(String[] args) { 
         // Write your code here
         Scanner W=new Scanner(System.in);
+        System.out.print("Enter the String");
         String str=W.next();
         //String Int=W.next();
         System.out.println(str);
