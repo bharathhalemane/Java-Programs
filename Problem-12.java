cf0046bd79840a1671c9df7d51313854cf21573f
Bharath Halemane H V <141356210+bharathhalemane@users.noreply.github.com>
2025-02-16 21:03:49 +0530

    Basic Arithmetic

--- Problem-12.java
+++ Problem-12.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class project12 {
+public class Problem_12 {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter value A:");        
