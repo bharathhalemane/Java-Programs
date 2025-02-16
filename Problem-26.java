162d30da78a64a7215eabcbaf0581b220e884b10
Bharath Halemane H V <141356210+bharathhalemane@users.noreply.github.com>
2025-02-16 21:05:38 +0530

    Eligible criteria - 4

--- Problem-26.java
+++ Problem-26.java
@@ -3,7 +3,7 @@
 
 import java.util.Scanner;
 
-class Problem26{
+class Problem_26{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the Maths marks: ");
