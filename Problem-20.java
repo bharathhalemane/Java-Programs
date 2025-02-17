c97ca01c5492b79674ded02daac1b8b2d877f6a7
Bharath Halemane H V <141356210+bharathhalemane@users.noreply.github.com>
2025-02-18 00:00:42 +0530

    Exam Result

--- Problem-20.java
+++ Problem-20.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class project20{
+class Problem_20{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the marks out of 100:");
