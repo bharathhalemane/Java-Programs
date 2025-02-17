86d46747c9d50ebc154ed18a9f19138f0cc3b505
Bharath Halemane H V <141356210+bharathhalemane@users.noreply.github.com>
2025-02-18 00:00:04 +0530

    Masking Password

--- Problem-19.java
+++ Problem-19.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class project19{
+class Problem_19{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter a Password:");
