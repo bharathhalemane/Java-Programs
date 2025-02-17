7e8e24f53c73a5007883f1a1c3386cf465eaf67f
Bharath Halemane H V <141356210+bharathhalemane@users.noreply.github.com>
2025-02-17 23:58:23 +0530

    Valid Password

--- Problem-17.java
+++ Problem-17.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class project17{
+class Problem_17{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter a password contain more than 7 characters:");
