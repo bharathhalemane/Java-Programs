f2de8b159f9e4e891219c6552c267b99d9ee2b97
Bharath Halemane H V <141356210+bharathhalemane@users.noreply.github.com>
2025-02-18 22:17:49 +0530

    Eligible criteria - 1

--- Problem-23.java
+++ Problem-23.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 //Given a student has scored marks in maths, physics and chemistry.
 //if a student is eligible for admission in a professional course.
 //conditions: 1) M>=70 and P >= 60 and C >= 60 2) M+C+P >= 180
-class project23{
+class Problem_23{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the marks of Maths:");
