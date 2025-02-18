7d97c1529252b7db39f742990cc52fc2fd676861
Bharath Halemane H V <141356210+bharathhalemane@users.noreply.github.com>
2025-02-18 22:19:52 +0530

    Sum of two numbers

--- Problem-7.java
+++ Problem-7.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class project7{
+class Problem_7{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the first num:");
