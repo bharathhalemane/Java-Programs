60b4f85748f8ba6666a56d945f5b47a08829ca0b
Bharath Halemane H V <141356210+bharathhalemane@users.noreply.github.com>
2025-02-18 22:15:31 +0530

    skipping letters

--- Problem-21.java
+++ Problem-21.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class project21{
+class Problem_21{
     public static void main(String[] args){         
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the String:");        
